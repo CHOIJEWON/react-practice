cc654ac1d6c73e2727021d63127b6c463e5788df
CHOIJEWON <105709970+CHOIJEWON@users.noreply.github.com>
2023-12-28 11:10:33 +0900

    [feat] React Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+/**
+ * Component
+ * 1. 컴포넌트는 html의 확장성을 높여준다
+ * 2. 컴포넌트의 적절한 분리는 확장성 뿐만이 아닌 코드의 가시성을 높여준다
+ * 3. 컴포넌트의 첫글자는 대문자를 철칙으로 한다
+ */
+
+function Header(){
+  return <header>
+  <h1><a href="/">React</a></h1>
+  </header>
+}
+
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a href='/read/1'>html</a></li>
+      <li><a href='/read/2'>css</a></li>
+      <li><a href='/read/3'>js</a></li>
+    </ol>
+  </nav>
+}
+
+function Article(){
+  return <article>
+    <h2> Welcome </h2>
+    Hello, Web
+  </article>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
